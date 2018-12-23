1b9145d5ae56194e7a6c16c9003adf949c9ac381
khubbard96 <kevlar2020@gmail.com>
2018-12-22 20:28:10 -0600

    starting object

--- core.js
+++ core.js
@@ -1,1 +1,29 @@
 console.log("core");
+
+List = function() {
+  this._id = 0;
+  this._name = 0;
+  this._tags = [];
+  this._items = [];
+  this._html = "";
+
+  this._setup = function() {
+
+  }
+
+  this._buildHtml = function() {
+    
+  }
+
+  this._buildHtml();
+
+  this._setup();
+}
+
+List.prototype.addItem = function() {
+
+}
+
+List.prototype.getHtml = function() {
+  return this._html;
+}
